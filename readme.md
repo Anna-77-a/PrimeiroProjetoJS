3ae337906ea63163c356c846e3d40e1bb9537f63
ANNA PAULA HENRIQUES SOUZA DIAS <26171895@edu119.local>
2026-07-30 14:39:02 -0300

    Arquivos criados app, operadores, variavel e oinformação

--- readme.md
+++ readme.md
@@ -14,3 +14,13 @@ Este projeto foi desenvolvido durante a primeira aula de JavaScript.
 
 O objetivo foi aprender:
 - utilizar o VS code;
+- executar o Javascript com Node.js
+- utilizar o Terminal
+- Criar documento utilizando Markdown
+
+---
+git status
+git add .
+git status
+git commit -m "Arquivo criado app, operadores, variavel e informação"
+git push
---
+++ variavel.js
@@ -0,0 +1,36 @@
+// let =   valor ser alterado;
+//         utilizado para atualizações;
+//         // ideal para contadores e loops;
+const =     valor e fixo - não pode ser alterado;
+            mais seguro e previsível;
+            preferido pelas empresas;
+
+
+var = escopo confuso;
+    pode ser redeclado;
+    gera bugs silenciosos;
+
+
+let a = "Iago";
+let nomeAluno = "Iago"; // Nome descritivo
+
+let x = 15;
+let quantidadeAluno = 15; // Nome descritivo
+
+// String
+//  "João"
+
+// Number
+// 20 1.80
+
+// // Boolean
+// true false
+
+// undefined
+// let x;
+
+// null
+// let telefone = null
+
+let nome="Vitor";
+nome
